ccadd002a4591c818e05f9c7b119e24596540a9a
xseven <99634076+xseven-gt@users.noreply.github.com>
2022-04-05 12:46:18 +0700

    Create prohek.py

---
+++ prohek.py
@@ -0,0 +1,55 @@
+import time
+
+import os
+
+import colorsys
+
+print(" Wassap Madafakah ")
+
+os.system("clear")
+
+print("Hechking Akun Xenzaxnxx18++")
+
+time.sleep(3)
+
+print("Meowbah IP : 124.36.269.82")
+
+time.sleep(3)
+
+print("Meowbah Email : xenzapedo@gmail.com")
+
+time.sleep(3)
+
+print("Meowbah Password : XenzaPedo133")
+
+time.sleep(3)
+
+print (" ")
+
+print(" [XNXX] HECKING PRO WIBU MENU HECKING [XNXX] ")
+
+print(" ")
+
+print(" ")
+
+print(" 1. Login [XenzaXnxx] Discord ")
+
+print(" 2. Attack [XenzaPedo] IP ")
+
+print(" 3. Hengking [XenzaXXX] Account ")
+
+print(" ")
+
+print(" Your Choice : ")
+
+print(" ")
+
+print(" HENGKING XENZA ACCOUNT ")
+
+print(" ")
+
+print(" Login Xenza18+ Pro Max Account... ")
+
+print(" ")
+
+print(" Succes Hecking Account... ")
